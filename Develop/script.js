438f31e569d4dd50565480ea41add9f1a17bd97f
PHelberg <121986315+PHelberg@users.noreply.github.com>
2023-02-20 12:47:47 -0600

    Third revision of generator, still not displaying.

--- Develop/script.js
+++ Develop/script.js
@@ -11,17 +11,33 @@ function writePassword() {
 }
 // This function generates the random content
 function generatePassword() {
-var length = prompt("Enter desired length your password.");
+var length = parseInt(prompt("Enter desired length of your password (between 8 and 128"));
+if (isNaN(length) || length < 8 || length > 128) { 
+alert("Please enter a number between 8 and 128.");
+return;
+}
+
 var lower = confirm("Do you want this to include lower case letters?");
 var upper = confirm("Do you want this to include upper case letters?");
 var numeric = confirm("Do you want this to include numeric values?");
-
 var special = confirm("Do you want this to include any special characters?");
+if (!lower && !upper && !numeric && !special)
+{alert("I can't generate a blank password"); 
+}
+
 var criteria = "";
   if (lower) criteria += "abcdefghijklmnopqrstuvwxyz";
   if (upper) criteria += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   if (number) criteria += "0123456789";
   if (special) criteria += "!@#$%^&*()_+-=[]{}|;':\"<>,.?/\\";
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(math.random() * criteria.length);
+    password += criteria[randomIndex];
+  }
+
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
